0c2b86ce75468df9f9ef4e77c647073c7497b9e0
RoxineAnneRegalado <146078207+RoxineAnneRegalado@users.noreply.github.com>
2024-11-06 23:26:46 -0800

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ubuntu server-PHP Deployment</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+	<p>Ubuntu server -PHP Deployment</p>
+
+</body>
+
+
+<div>
+	
+	<label>F-name:</label>
+	<input type="text" name="fname"class="form-control">
+
+</div>
+
+<div>
+	
+	<label>m-name:</label>
+	<input type="text" name="mname"class="form-control">
+</div>
+
+<div>
+	
+	<label>l-name:</label>
+	<input type="text" name="lname"class="form-control">
+</div>
+
+<div>
+	
+	<label>age:</label>
+	<input type="text" name="age"class="form-control">
+	
+</div>
+
+<button>enter</button>
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
